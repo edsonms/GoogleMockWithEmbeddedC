2592d57ad96817fa5a9a73f53fbbe73a06778be4
Edson Manoel da Silva <edson.eelen@gmail.com>
2023-01-01 00:00:00 -0300

    Improves test messages

--- test/src/test_gpio.cpp
+++ test/src/test_gpio.cpp
@@ -13,8 +13,12 @@
 #include <gmock/gmock.h> 
 #include "gpio_mock.hpp"
 
+#include <iostream>
+
+
 using ::testing::_;
 using ::testing::Return;
+using namespace std;
 
 namespace GpioTests
 {
@@ -49,27 +53,62 @@ namespace GpioTests
     TEST_F(GpioAPIsTest,TestConfigureGPIO)
     {
     //List of tests to be done for function TestConfigureGPIO
-    //1 - Test returning success from gpio_config
+    //1 - Test returning success from gpio_config and gpio_set_level
     //2 - Test returning error from gpio_config
+    //3 - Test returning error from gpio_set_level
+    bool TestHasError = 0;
     esp_err_t test_return = ESP_ERR_INVALID_ARG;
 
-    //Test 1
+    //////////////////////////////////////////////// Test 1 /////////////////////////////////////////////////////////////////////////////////////
+	cout << endl << "[   INFO   ] Test 1 - Test returning success from gpio_config and gpio_set_level" << endl;
     EXPECT_CALL(*gpioMockObj,gpio_config(_)) //The called argument is a pointer, so we will test call expectation with the any argument pararameter
+    .Times(1)
+    .WillOnce(Return(ESP_OK));
+    EXPECT_CALL(*gpioMockObj,gpio_set_level(GPIO_NUM_2,LOW))
     .Times(1)
     .WillOnce(Return(ESP_OK));
 
     test_return = ConfigureGPIO(GPIO_NUM_2);
-    EXPECT_EQ(ESP_OK, test_return);
+    EXPECT_EQ(ESP_OK, test_return) << (TestHasError = true); //If expectation fails, set TestHasError to true;
+    if (TestHasError)
+        std::cout << "[   FAIL   ] " << "Test 1 fail" << endl;
+    else
+        std::cout << "[    OK    ] " << "Test 1 pass" <<endl;         
 
-    //Test 2
-    test_return = ESP_ERR_INVALID_ARG;
+    //////////////////////////////////////////////// Test 2 /////////////////////////////////////////////////////////////////////////////////////
+    TestHasError = 0;
+    test_return = ESP_OK;
+	cout << endl << "[   INFO   ] Test 2 - Test returning error from gpio_config" << endl;
 
     EXPECT_CALL(*gpioMockObj,gpio_config(_)) //The called argument is a pointer, so we will test call expectation with the any argument pararameter
     .Times(1)
     .WillOnce(Return(ESP_ERR_INVALID_ARG));
 
-    test_return = ConfigureGPIO((gpio_num_t)(GPIO_NUM_39));
-    EXPECT_EQ(ESP_ERR_INVALID_ARG, test_return);
+    test_return = ConfigureGPIO((gpio_num_t)(GPIO_NUM_38));
+    EXPECT_EQ(ESP_ERR_INVALID_ARG, test_return) << (TestHasError = true); //If expectation fails, set TestHasError to true;
+    if (TestHasError)
+        std::cout << "[   FAIL   ] " << "Test 2 fail" << endl;
+    else
+        std::cout << "[    OK    ] " << "Test 2 pass" <<endl;     
+
+    //////////////////////////////////////////////// Test 3 /////////////////////////////////////////////////////////////////////////////////////
+    TestHasError = 0;
+    test_return = ESP_OK;
+	cout << endl << "[   INFO   ] Test 3 - Test returning error from gpio_set_level" << endl;
+
+    EXPECT_CALL(*gpioMockObj,gpio_config(_)) //The called argument is a pointer, so we will test call expectation with the any argument pararameter
+    .Times(1)
+    .WillOnce(Return(ESP_OK));
+    EXPECT_CALL(*gpioMockObj,gpio_set_level(GPIO_NUM_39,LOW))
+    .Times(1)
+    .WillOnce(Return(ESP_ERR_INVALID_ARG));
+
+    test_return = ConfigureGPIO(GPIO_NUM_39);
+    EXPECT_EQ(ESP_ERR_INVALID_ARG, test_return) << (TestHasError = true); //If expectation fails, set TestHasError to true;
+    if (TestHasError)
+        std::cout << "[   FAIL   ] " << "Test 3 fail" << endl;
+    else
+        std::cout << "[    OK    ] " << "Test 3 pass" <<endl << endl;       
     
     }
 
@@ -78,7 +117,7 @@ namespace GpioTests
     //List of tests to be done for function ToggleGpio
     //1 - Test toggling from LOW to HIGH   
     //2 - Test toggling from HIGH to LOW
-    //3 - Test if passing invalid pin number returns an error
+    //3 - Test gpio_set_level returning error
     
     }
 }
